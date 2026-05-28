ec92eef092df935629cb94da8fc7e50559ea5c6b
lucas-avv <mmaxlucas16@gmail.com>
2026-05-28 20:35:13 -0300

    feat: cria serializers, views e rotas do DRF para a api de produtos

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from app.models import Produto
+from app.serializers import ProdutoSerializer
 
-
+# lucas -  CRUD completo de produtos
+class ProdutoViewSet(viewsets.ModelViewSet):
+    # Puxa todos os produtos do banco
+    queryset = Produto.objects.all()
+    # Diz qual tradutor (serializer) vai ser usado
+    serializer_class = ProdutoSerializer
+    serializer_class = ProdutoSerializer
